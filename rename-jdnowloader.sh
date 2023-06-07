baa021eb2f017446be21aab69ec8c23953cb095e
Andres Gongora <mail@andresgongora.com>
2023-06-07 09:55:45 +0200

    Add more renaming rules
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

--- rename-jdnowloader.sh
+++ rename-jdnowloader.sh
@@ -4,6 +4,7 @@ readonly DIR=${1:-"."}
 readonly SED_VERSION=$(sed --version | head -n 1 | sed 's|[^0-9\.]||g')
 
 find "${DIR}/" -type f -print0 | while IFS= read -r -d '' file; do
+    #echo "$file"
 	file_name=$(basename -- "$file")
 	dir_name=$(dirname -- "$file")
 
@@ -12,14 +13,16 @@ find "${DIR}/" -type f -print0 | while IFS= read -r -d '' file; do
     ## ?; -> remove
     ## (bitrate_kbits) -> remove
     ## hls -> remove
-    ## _ -> space | multiple spaces -> one space | trailing spaces -> remove
+    ## _ -> space | multiple spaces -> one space | trailing spaces -> remove | leading spaces
 	new_file_name=$(echo "$file_name" | sed -r \
 			-e 's|[^[:alnum:][:punct:][:space:]]||g' \
 			-e 's|1080p||g; s|720p||g; s|hls_1||g; s|hls||g;' \
+			-e 's|\( ?[0-9]{2,}fps ?[^\)]*\w\)||g;' \
+			-e 's|\( ?[0-9]{3,}p ?[^\)]*\w\)||g;' \
 			-e 's|_[0-9]\.|.|g' \
 			-e 's|[|¿\?\;\¡\!]||g' \
 			-e 's|\ ?\([0-9]\w*_\w*\)\.|\.|g' \
-			-e 's|_| |g; s|\ +|\ |g; s|\ \.|\.|g; ')
+			-e 's|_| |g; s|\ +|\ |g; s|\ \.|\.|g; s|^\ ||g;')
 
 	if [[ "$new_file_name" != "$file_name" ]]; then
 		new_name="${dir_name}/${new_file_name}"
