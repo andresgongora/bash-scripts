4269f82f0e22ac08ad44ecf3d6b4e1195b93de3e
Andres Gongora <mail@andresgongora.com>
2023-09-20 13:08:11 +0200

    Add audio compressor for podcasts
    Signed-off-by: Andres Gongora <mail@andresgongora.com>

---
+++ podcast-audio-compressor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+function compress(){
+    local readonly INPUT="$1"
+    local readonly extension="${INPUT##*.}"
+    local readonly OUTPUT="$(basename "${INPUT}" ".$extension").PAC.mp3"
+
+    local readonly FILTER="\
+        compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0, \
+        adeclip, \
+        adeclick, \
+        lowpass=7000, \
+        highpass=150, \
+        loudnorm=I=-16:LRA=7:tp=-2.0:print_format=none"
+
+    ffmpeg -y -i "$INPUT" -af "${FILTER}" -codec:a libmp3lame -qscale:a 1 "$OUTPUT" </dev/null
+}
+
+
+readonly DIR=${1:-"."}
+find "${DIR}/" -type f -print0 | while IFS= read -r -d '' file; do
+    #echo "$file"
+    extension=${file##*.}
+	file_name=$(basename -- "$file" ".$extension")
+	dir_name=$(dirname -- "$file")
+
+	if [[ "$file_name" != *.PAC ]]; then
+            echo "--------------------------------------------------"
+            echo "Dynamic-Range Compressing and normalizing ${INPUT}"
+            echo "--------------------------------------------------"
+            echo ""
+	        compress "${file}" # && trash "$file"
+            trash "${file}"
+            echo "\n\n\n"
+	fi
+done
+
+
